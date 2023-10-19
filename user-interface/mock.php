57a7e2dbb4198a893719b50dc52ad329049795ed
Minmin <minmin@localhost.localdomain>
2023-10-19 10:07:09 +0800

    Installed jQuery and Bootstrap.

---
+++ user-interface/mock.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mock</title>
+    <!-- import bootstrap -->
+    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.css" />
+    <script src="../vendor/components/jquery.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a7d47cd4b173d8fcf2336b892c575b5e2a5b0439',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,11 +11,20 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a7d47cd4b173d8fcf2336b892c575b5e2a5b0439',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'components/jquery' => array(
+            'pretty_version' => 'v3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => '8edc7785239bb8c2ad2b83302b856a1d61de60e7',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
